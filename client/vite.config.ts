95629bd9f81a823320f83de163e7d0778c3dc564
Arthur Peruzzo <peruzzoarthur@gmail.com>
2024-08-03 08:28:31 -0300

    fixed overflow in projects card, adding ga

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,26 +1,37 @@
-import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from 'vite'
+import { sentryVitePlugin } from '@sentry/vite-plugin'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
-
+import { VitePluginRadar } from 'vite-plugin-radar'
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react(), TanStackRouterVite(), sentryVitePlugin({
-        org: "sp3rzo",
-        project: "javascript-react"
-    }), sentryVitePlugin({
-        org: "sp3rzo",
-        project: "arthur-portfolio"
-    })],
+export default ({ mode }) => {
+    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+    console.log(process.env.VITE_GOOGLE_TAG)
+    return defineConfig({
+        plugins: [
+            react(),
+            TanStackRouterVite(),
+            sentryVitePlugin({
+                org: 'sp3rzo',
+                project: 'arthur-portfolio',
+            }),
+            VitePluginRadar({
+                // Google Analytics tag injection
+                analytics: {
+                    id: process.env.VITE_GOOGLE_TAG,
+                },
+            }),
+        ],
 
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './src'),
+            },
         },
-    },
 
-    build: {
-        sourcemap: true
-    }
-})
+        build: {
+            sourcemap: true,
+        },
+    })
+}
