68118b34c704d6bc65fa3eccfa9c4129c34aa443
Arthur Peruzzo <peruzzoarthur@gmail.com>
2024-08-03 08:41:26 -0300

    ga

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,33 +5,29 @@ import path from 'path'
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 import { VitePluginRadar } from 'vite-plugin-radar'
 // https://vitejs.dev/config/
-export default ({ mode }) => {
-    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
-    console.log(process.env.VITE_GOOGLE_TAG)
-    return defineConfig({
-        plugins: [
-            react(),
-            TanStackRouterVite(),
-            sentryVitePlugin({
-                org: 'sp3rzo',
-                project: 'arthur-portfolio',
-            }),
-            VitePluginRadar({
-                // Google Analytics tag injection
-                analytics: {
-                    id: process.env.VITE_GOOGLE_TAG,
-                },
-            }),
-        ],
-
-        resolve: {
-            alias: {
-                '@': path.resolve(__dirname, './src'),
+export default defineConfig({
+    plugins: [
+        react(),
+        TanStackRouterVite(),
+        sentryVitePlugin({
+            org: 'sp3rzo',
+            project: 'arthur-portfolio',
+        }),
+        VitePluginRadar({
+            // Google Analytics tag injection
+            analytics: {
+                id: 'G-PP0YRSWYBY',
             },
-        },
+        }),
+    ],
 
-        build: {
-            sourcemap: true,
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
         },
-    })
-}
+    },
+
+    build: {
+        sourcemap: true,
+    },
+})
